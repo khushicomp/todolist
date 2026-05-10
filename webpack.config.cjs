9b975cf22683af3c2d1f6a62f6f233b3502c1834
khushicomp <khushiyuwanathe8@gmail.com>
2026-05-10 11:16:47 +0530

    feat: update package.json and webpack.config.cjs for deployment setup

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/todolist/",
   },
   devServer: {
     static: "./dist",
